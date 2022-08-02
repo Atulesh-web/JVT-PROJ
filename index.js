5deeaf09dcf9093ad54c32dc4ae4f1ef366c3c20
Atulesh-web <57455225+Atulesh-web@users.noreply.github.com>
2022-08-02 15:44:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'App';
-import Testimonials from 'Testimonials';
-import Navbar from "Navbarr";
+import App from './App';
+import Testimonials from './Testimonials';
+import Navbar from "./Navbarr";
 
 ReactDOM.render(<Navbar />, document.getElementById("navbarSupportedContent"));
 ReactDOM.render(<Testimonials />, document.getElementById("press"));
